e3b03534361830ca64c144f2d68395a6612de2ad
Felix Thaler <thaler@cscs.ch>
2020-07-14 11:28:03 +0200

    Removed hardcoded loop var

--- daceperiments/transforms.py
+++ daceperiments/transforms.py
@@ -32,45 +32,50 @@ class BasicRegisterCache(Transformation):
         for state in states:
             for edge in state.edges():
                 src, dst = edge.src, edge.dst
-                if (isinstance(src, nodes.AccessNode) and src.data == 'buf'
+                if (isinstance(src, nodes.AccessNode)
+                        and src.data == self.array
                         or isinstance(dst, nodes.AccessNode)
-                        and dst.data == 'buf'):
+                        and dst.data == self.array):
                     yield edge.data
 
-    def _get_offsets(self, states):
+    def _get_offsets(self, states, loop_var):
         min_offset, max_offset = 1000, -1000
         for memlet in self._buffer_memlets(states):
             rb, re, _ = memlet.subset.ranges[0]
-            rb_offset = rb - symbolic.symbol('i')
-            re_offset = re - symbolic.symbol('i')
+            rb_offset = rb - symbolic.symbol(loop_var)
+            re_offset = re - symbolic.symbol(loop_var)
             min_offset = min(min_offset, rb_offset, re_offset)
             max_offset = max(max_offset, rb_offset, re_offset)
         return min_offset, max_offset
 
-    def _make_indices_absolute(self, states, min_offset):
+    def _make_indices_absolute(self, states, loop_var, min_offset):
         for memlet in self._buffer_memlets(states):
-            memlet.subset.replace({symbolic.symbol('i'): -min_offset})
+            memlet.subset.replace({symbolic.symbol(loop_var): -min_offset})
 
-    def _insert_shift_after(self, sdfg, state, min_offset, max_offset):
-        shift_state = sdfg.add_state_after(state)
-        buf_read = shift_state.add_read(self.array)
-        buf_write = shift_state.add_write(self.array)
+    def _insert_shift_after(self, sdfg, states, min_offset, max_offset):
+        for state in states:
+            shift_state = sdfg.add_state_after(state)
+            buf_read = shift_state.add_read(self.array)
+            buf_write = shift_state.add_write(self.array)
 
-        shift_state.add_edge(
-            buf_read, None, buf_write, None,
-            dace.Memlet.simple(self.array,
-                subset_str=f'1:{max_offset - min_offset + 1}',
-                other_subset_str=f'0:{max_offset - min_offset}'))
+            shift_state.add_edge(
+                buf_read, None, buf_write, None,
+                dace.Memlet.simple(
+                    self.array,
+                    subset_str=f'1:{max_offset - min_offset + 1}',
+                    other_subset_str=f'0:{max_offset - min_offset}'))
 
     def apply(self, sdfg: dace.SDFG):
         before_state = sdfg.node(self.subgraph[self._before_state])
         loop_state = sdfg.node(self.subgraph[self._loop_state])
         guard_state = sdfg.node(self.subgraph[self._guard_state])
+        loop_var = next(iter(sdfg.in_edges(guard_state)[0].data.assignments))
 
-        min_offset, max_offset = self._get_offsets([before_state, loop_state])
-        self._make_indices_absolute([before_state, loop_state], min_offset)
+        compute_states = [before_state, loop_state]
 
-        sdfg.arrays[self.array].shape = (max_offset - min_offset + 1,)
+        min_offset, max_offset = self._get_offsets(compute_states, loop_var)
+        self._make_indices_absolute(compute_states, loop_var, min_offset)
 
-        self._insert_shift_after(sdfg, before_state, min_offset, max_offset)
-        self._insert_shift_after(sdfg, loop_state, min_offset, max_offset)
+        sdfg.arrays[self.array].shape = (max_offset - min_offset + 1, )
+
+        self._insert_shift_after(sdfg, compute_states, min_offset, max_offset)
